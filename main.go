4a4aa6fb2d5f66412541a94aae9a05bdbb9ac053
pchchv <ipchchv@gmail.com>
2022-09-17 11:54:53 +0300

    Implement getting Russian&English country names

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
+	"io"
+	"log"
+	"os"
 
 	"github.com/biter777/countries"
 )
@@ -14,6 +18,36 @@ func getEmoji(countryName string) (string, error) {
 	}
 	emoji = c.Emoji()
 	return emoji, nil
+}
+
+func getCountryNames(name string) (string, string) {
+	var country []string
+	file, err := os.Open("_countries.csv")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.Comma = ';'
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				log.Println("_______EOF_______")
+				break
+			}
+			log.Panic(err)
+			break
+		}
+		if record[1] == name || record[4] == name {
+			country = record
+			break
+		}
+	}
+	if country == nil {
+		log.Panic(errors.New("Incorrect country name"))
+	}
+	return country[1], country[4]
 }
 
 func main() {
