a6fcd0b482d290392022f9694a465aa3c9e1404b
pchchv <ipchchv@gmail.com>
2022-09-18 11:26:49 +0300

    Implement a bot

--- bot.go
+++ bot.go
@@ -1,3 +1,15 @@
 package main
 
-func bot() {}
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func bot() {
+	bot, err := tgbotapi.NewBotAPI(getEnvValue("BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+}
